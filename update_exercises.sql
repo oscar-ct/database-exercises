e9fe5530d2f58c870f74d23b24f2791f9a9b66c3
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-19 09:52:51 -0500

    feat: updated records using UPDATE command and also created queries with captions

---
+++ update_exercises.sql
@@ -0,0 +1,21 @@
+
+USE codeup_test_db;
+
+
+SELECT * FROM albums;
+
+
+SELECT name AS 'All albums in the table' FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT sales FROM albums;
+
+
+SELECT name AS 'All albums released before 1980' FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+SELECT release_date FROM albums WHERE release_date < 1900;
+
+
+SELECT name AS 'All albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums WHERE artist = 'Peter Jackson'
+
