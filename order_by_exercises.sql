3206e9fd2d1e078a306b147e64ffba0946b4b65f
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-19 16:00:31 -0500

    feat: completed ORDER BY clauses by refactoring WHERE clauses form previous exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,16 @@
+USE employees;
+
+
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name;
+
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name, last_name;
+
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name, first_name;
+
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name DESC, first_name DESC;
+
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT DISTINCT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%'
