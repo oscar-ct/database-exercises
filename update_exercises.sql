68d4815ea870c9a4c7428b9074ac193d519e3816
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-19 11:20:06 -0500

    feat: removed extra query selector

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,17 +3,17 @@ USE codeup_test_db;
 
 
 
-SELECT name, sales AS 'All albums in the table' FROM albums;
+SELECT name AS 'All albums in the table' FROM albums;
 UPDATE albums SET sales = sales * 10;
 SELECT name, sales FROM albums;
 
 
-SELECT name, release_date AS 'All albums released before 1980' FROM albums WHERE release_date < 1980;
+SELECT name AS 'All albums released before 1980' FROM albums WHERE release_date < 1980;
 UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
 SELECT name, release_date FROM albums WHERE release_date < 1900;
 
 
-SELECT name, artist AS 'All albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+SELECT name AS 'All albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 SELECT name, artist FROM albums WHERE artist = 'Peter Jackson'
 
