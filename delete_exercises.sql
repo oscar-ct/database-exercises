3534e521b73ab90722c0cfb8a9ae2014f810f629
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-19 11:19:36 -0500

    feat: removed additional query selector

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
-SELECT name, release_date AS 'Albums released after 1991' FROM albums WHERE release_date > 1991;
+SELECT name AS 'Albums released after 1991' FROM albums WHERE release_date > 1991;
 DELETE FROM albums WHERE release_date > 1991;
 
 
-SELECT name, genre AS 'Albums with the genre ''disco''' FROM albums WHERE genre = 'disco';
+SELECT name AS 'Albums with the genre ''disco''' FROM albums WHERE genre = 'disco';
 DELETE FROM albums WHERE genre = 'disco';
 
-SELECT name, artist AS 'Albums by Whitney' FROM albums WHERE artist = 'Whitney';
+SELECT name AS 'Albums by Whitney' FROM albums WHERE artist = 'Whitney';
 DELETE FROM albums WHERE artist = 'Whitney';
 
 
