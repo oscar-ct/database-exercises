fc7e1129b088c796c1a127d1ce4abbdf650d2667
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-20 13:28:07 -0500

    feat: added comments to clarify problem numbers and removed unused code

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,18 +1,18 @@
 USE employees;
 
 
+#/////////////////////////// Problem #2  ///////////////////////////
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name;
 
+#/////////////////////////// Problem #3  ///////////////////////////
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name, last_name;
 
+#/////////////////////////// Problem #4  ///////////////////////////
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name, first_name;
 
-SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no;
+#/////////////////////////// Problem #5  ///////////////////////////
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no; #ASC;
 
+#/////////////////////////// Problem #6  ///////////////////////////
 SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no DESC;
 
-
-# SELECT * FROM employees WHERE last_name LIKE 'E%';
-# SELECT * FROM employees WHERE last_name LIKE '%q%';
-# SELECT DISTINCT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-# SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%'
