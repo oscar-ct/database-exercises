55b50ee6bfd25449c63fc5e74c8311307c0cfd48
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-20 13:18:53 -0500

    feat: refactored exercises to reflect accurate queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -7,10 +7,12 @@ SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_na
 
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name, first_name;
 
-SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name DESC, first_name DESC;
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no;
+
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no DESC;
 
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
-SELECT DISTINCT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%'
+# SELECT * FROM employees WHERE last_name LIKE 'E%';
+# SELECT * FROM employees WHERE last_name LIKE '%q%';
+# SELECT DISTINCT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+# SELECT * FROM employees WHERE last_name NOT LIKE '%qu%' AND last_name LIKE '%q%'
